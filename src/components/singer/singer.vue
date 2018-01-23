85f9d71dc1b686678e25d9347f6f87018d65f4c1
jby <1614963384@qq.com>
2018-01-23 16:35:10 +0800

    歌手页面后端数据的获取;对数据进行排序

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,12 +1,84 @@
 <template>
   <div class="singer" ref="singer">
-    歌手页面
+
   </div>
 </template>
 
 <script>
-  export default {
+import {getSingerList} from '../../api/singer'
+import Singer from '../../common/js/singer'
 
+const HOT_NAME='推荐'
+const HOT_SINGER_LEN=10
+  export default {
+    data() {
+      return {
+        singerList:[],
+        map:[]
+      }
+    },
+    created() {
+      this._getSingerList();
+    },
+    methods:{
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if(res.code===0) {
+ //           console.log(this.singerLsit);
+            this.singerLsit=this._nowListMap(res.data.list);
+            console.log(this.singerLsit);
+          }
+        })
+      },
+//      将得到的singerList进行排列,调整成我们需要的数据:即:热门-a-z
+      _nowListMap(list) {
+//        创建对象map,用来归类singerList
+        let map={
+          hot:{
+            title:HOT_NAME,
+            items:[]
+          }
+        }
+        list.forEach((item,index) => {
+//          添加hot的列表数据
+          if(index < HOT_SINGER_LEN) {
+//            item中只有三项是所需内容,只取这3项,其中有一项是图片链接,所有item都会用到,封装成对象Singer
+            map.hot.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }))
+          }
+//          依据Findex来在map中建立方法
+          const key=item.Findex
+          if(!(map[key])){
+            map[key] = {
+              title:key,
+              items:[]
+            }
+          }
+          map[key].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        })
+//        将对象转化成有序数组
+        let hot=[]
+        let ret=[]
+        for(let key in map){
+          let val=map[key]
+          if(val.title.match(/[a-zA-Z]/)){
+            ret.push(val)
+          }else if(val.title===HOT_NAME){
+            hot.push(val)
+          }
+        }
+//        ret进行排序
+        ret.sort((a,b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
+      },
+    }
   }
 
 </script>
