dd66d1d038d2513eb814229aa639971892d23f83
jby <1614963384@qq.com>
2018-01-24 19:15:23 +0800

    右侧导航栏连续的情况下,左侧内容相应变化

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -13,7 +13,7 @@
    </ul>
     <div class="list-shortcut">
       <ul>
-        <li class="item" v-for="(item,index) in shortCutList" :data-index="index" @touchstart.stop.prevent="onShortcutTouchStart">{{item}}
+        <li class="item" v-for="(item,index) in shortCutList" :data-index="index" @touchstart.stop.prevent="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">{{item}}
         </li>
       </ul>
     </div>
@@ -24,6 +24,10 @@
   import scroll from '../scroll/scroll.vue'
   import {getData}from '../../common/js/dom'
   export default {
+    created() {
+//      建立touch变量仅限于在created中使用
+      this.touch={}
+    },
     props:{
       data:{
         type:Array,
@@ -44,9 +48,30 @@
     methods:{
 //      首次点击右边导航,左边内容跳转至对应模块
       onShortcutTouchStart(e) {
+//        获得滑动时起始位置的index
         let anchorIndex = getData(e.target, 'index')
-        console.log(anchorIndex);
-        this.$refs.listview.scrollToElement(this.$refs.listGroup[anchorIndex],0)
+        let firstTouch=e.touches[0];
+//        获得滑动起始位置的方向像素
+        this.touch.y1=firstTouch.pageY;
+        this.touch.anchorIndex=anchorIndex;
+//        让左侧内容跳转至相应模块
+        this. _scrollTo(anchorIndex);
+
+      },
+//      为了获取滑动结束后的index值
+      onShortcutTouchMove(e) {
+        //        获得滑动时起始位置的index
+        let firstTouch=e.touches[0];
+//        获得滑动起始位置的方向像素
+        this.touch.y2=firstTouch.pageY;
+//        滑动结束时的index是起始位置的index+(像素差值/每个index的像素值18)
+        let delta=Math.floor((this.touch.y2-this.touch.y1)/18)
+//        加parseInt的目的是,this.touch.anchorIndex是一个字符串,需要转化成数字
+        let anchorIndex =parseInt(this.touch.anchorIndex)+delta
+        this._scrollTo(anchorIndex);
+      },
+      _scrollTo(index) {
+        this.$refs.listview.scrollToElement(this.$refs.listGroup[index],0)
       }
     }
   }
