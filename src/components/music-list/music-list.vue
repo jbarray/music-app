bd46bc517b1aba0a53a6ce7febedfd6e683fa4c8
jby <1614963384@qq.com>
2018-02-06 18:01:37 +0800

    实现功能:歌手页面的随机播放按钮

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -3,7 +3,7 @@
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgstyle" ref="bgImage">
       <div class="play-wrapper">
-        <div ref="playBtn" v-show="songs.length>0" class="play">
+        <div ref="playBtn" v-show="songs.length>0" class="play" @click="randomPlay">
           <i class="icon-play"></i>
           <span class="text">随机播放全部</span>
         </div>
@@ -72,8 +72,15 @@
           index
         })
       },
+      //随机播放
+      random() {
+        this.randomPlay({
+          list: this.songs
+        })
+      },
       ...mapActions([
-        'selectPlay'
+        'selectPlay',
+        'randomPlay'
       ])
     },
     mounted(){
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,9 +1,34 @@
 import * as types from './mutation-types'
+import {playMode} from '../common/js/config'
+import {shuffle} from '../common/js/utill'
+function findIndex(list,song) {
+  return list.findIndex((item) => {
+    return item.id === song.id
+  })
+}
 
-export const selectPlay = function ({commit,state},{list,index}) {
-  commit(types.SET_SEQUENCE_LIST,list)
-  commit(types.SET_PLAYLIST, list)
+export const selectPlay = function ({commit, state}, {list, index}) {
+  commit(types.SET_SEQUENCE_LIST, list)
+  if (state.mode === playMode.random) {
+    let randomList = shuffle(list)
+    commit(types.SET_PLAYLIST, randomList)
+    index = findIndex(randomList, list[index])
+  } else {
+    commit(types.SET_PLAYLIST, list)
+  }
   commit(types.SET_CURRENT_INDEX, index)
+  commit(types.SET_FULL_SCREEN, true)
+  commit(types.SET_PLAYING_STATE, true)
+}
+
+export const randomPlay = function ({commit}, {list}) {
+  commit(types.SET_PLAY_MODE, playMode.random)
+  commit(types.SET_SEQUENCE_LIST, list)
+  //播放列表是打乱原来列表的顺序,重新洗牌得来的
+  let randomList = shuffle(list)
+  commit(types.SET_PLAYLIST, randomList)
+  //从randomList的第一个歌曲开始播放
+  commit(types.SET_CURRENT_INDEX, 0)
   commit(types.SET_FULL_SCREEN, true)
   commit(types.SET_PLAYING_STATE, true)
 }
